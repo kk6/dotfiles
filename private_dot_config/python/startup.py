2a81fb14edc5226403f5865c66ec02272dd67a93
kk6 <hiro.ashiya@gmail.com>
2021-06-21 10:37:53 +0900

    :wrench: Changed the location of the python history file to ~/.cache/python/history

--- private_dot_config/python/startup.py
+++ private_dot_config/python/startup.py
@@ -1,4 +1,24 @@
+import atexit
+import os
 import readline
 import rlcompleter
 
+# TAB complete
 readline.parse_and_bind("tab: complete")
+
+# Store interactive Python shell history in ~/.cache/python/history
+history_file = os.path.join(
+    os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/.cache")),
+    "python/history",
+    )
+try:
+    readline.read_history_file(history_file)
+    # default history len is -1 (infinite), which may grow unruly
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    pass
+atexit.register(readline.write_history_file, history_file)
+
+# cleanup
+del os, history_file, readline, rlcompleter, atexit
+
