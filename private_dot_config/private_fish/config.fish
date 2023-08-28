1f5e4a4efeca1191e337d04dc35cdfa09f941bb1
kk6 <hiro.ashiya@gmail.com>
2023-08-28 18:31:54 +0900

    :wrench: Update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -19,30 +19,12 @@ alias twitter "open -na 'Google Chrome' --args '--app=https://mobile.twitter.com
 alias deck "open -na 'Google Chrome' --args '--app=https://tweetdeck.twitter.com'"
 alias fig "docker-compose"
 
-# pyenv等のconfigがbrew doctorに引っかかってしまう問題の対応
-# https://qiita.com/komo/items/450180282766ffb250ba
-function brew
-    set -xl PATH $PATH # Protect global PATH by local PATH
-    if type -q pyenv; and contains (pyenv root)/shims $PATH
-        set -e PATH[(contains -i (pyenv root)/shims $PATH)]
-    end
-
-    command brew $argv
-end
-
-#set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-#set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
 set -x PATH /usr/local/opt/icu4c/bin $PATH
 set -x PATH /usr/local/opt/icu4c/sbin $PATH
 
 # direnv
 eval (direnv hook fish)
 
-# Since this `/usr/local/bin` has priority over the path setting by fisherman's pyenv plugin, I commented out.
-# `echo{ There is also `/usr/local/bin` properly behind pyenv.
-# (until that time it was defined twice before and after pyenv)
-#set -x PATH /usr/local/bin $PATH
-#
 set -x PYTHONUSERBASE $HOME/.local
 set -x PATH $PYTHONUSERBASE/bin $PATH
 
@@ -73,10 +55,6 @@ set -x PATH $GOPATH/bin $PATH
 
 # Rust
 set -x PATH $HOME/.cargo/bin $PATH
-
-# For conda
-# source (conda info --root)/etc/fish/conf.d/conda.fish
-
 
 # For fzf
 set -x FZF_DEFAULT_OPTS "--height 40% --reverse --border"
